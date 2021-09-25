a268ff343d2d750f0a4aecea320a643ddcdeb1a8
Matrim Pathak <63013617+MatrimPathak@users.noreply.github.com>
2021-09-25 13:36:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Student", "Full-Stack Developer", "Designer"],
+    strings: ["Student", "Full-Stack Developer", "Game Developer", "Designer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed(".typing-2", {
-    strings: ["Student", "Full-Stack Developer", "Designer"],
+    strings: ["Student", "Full-Stack Developer", "Game Developer", "Designer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
